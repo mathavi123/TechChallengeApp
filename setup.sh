e104bbe6140c9164b9284f1a6787596cd61e872e
mathavi123 <abi.math@gmail.com>
2022-04-18 16:47:22 +0530

    changed file

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-sudo yum update -y  
-#sudo apt install docker.io -y						
-#sudo usermod -aG docker ubuntu
-sudo yum install git -y
-sudo yum install golang-go -y
-cd /home/ec2-user; wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
+sudo apt update -y  
+sudo apt-get install git -y
+sudo apt install golang-go -y
+cd /home/ubuntu/ServianTechChallenge/; wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
 echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list;
-sudo yum update
-sudo yum install -y postgresql-10
-sudo yum install net-tools
-cd /home/ec2-user/TechChallengeApp
-./build.sh
+sudo apt update
+sudo apt install -y postgresql-10
+sudo apt install net-tools
+sudo apt install golang-rice -y
+cd /home/ubuntu/TechChallengeApp
+go get github.com/GeertJohan/go.rice
+go get github.com/GeertJohan/go.rice/rice
+sudo ./build.sh
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'changeme';"
-cd /home/ec2-user/TechChallengeApp/dist
+sudo cd /home/ubuntu/TechChallengeApp/dist
 ./TechChallengeApp updatedb
 
-sed -i 's/"ListenHost" = "localhost"/"ListenHost" = "0.0.0.0"/g'  /home/ec2-user/TechChallengeApp/dist/conf.toml
-nuhub ./TechChallengeApp serve &
+sed -i 's/"ListenHost" = "localhost"/"ListenHost" = "0.0.0.0"/g'  /home/ubuntu/TechChallengeApp/dist/conf.toml
+./TechChallengeApp serve &
